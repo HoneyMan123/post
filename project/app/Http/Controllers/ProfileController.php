7d4eb30850e819cc9daadcfa10e762e1b20b9b82
stepan <stepanponomarev086@gmail.com>
2024-05-22 23:03:27 +0700

    a

---
+++ project/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\postAmount;
+use App\Models\posts;
+use Illuminate\Support\Facades\DB;
+use Predis\Command\Redis\AUTH as RedisAUTH;
+use DataTables;
+class ProfileController extends Controller
+{
+    /**
+     * Display the user's profile form.
+     */
+
+
+
+
+
+
+
+    public function edit(Request $request): Response
+    {
+        return Inertia::render('Profile/Edit', [
+            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+    public function posts()
+    {
+
+
+return Inertia::render('Profile/profile');
+//return posts::all();
+    }
+
+    public function postsApi(){
+        $posts = DB::table('posts')->get();
+        return response()->json($posts);
+    }
+
+
+    public function postSubmision(Request $request){
+        //variables
+$lastAmountValue = DB::table('postAmount')->latest()->where('email', Auth::user()->email)->value('postAmount');
+        $userExistance = DB::table('postAmount')->where('email', Auth::user()->email)->exists();
+
+        //scripts
+        if($userExistance){
+            DB::table('postAmount')->where('email', Auth::user()->email)->increment('postAmount', 1);
+            $lastAmountValue = DB::table('postAmount')->latest()->where('email', Auth::user()->email)->value('postAmount');
+            DB::table('posts')->insert(['email'=> Auth::user()->email, 'postValue'=> $request->input('inputForm'), 'postAmount'=>$lastAmountValue, 'created_at'=>DB::raw('NOW()')]);
+
+        } else {
+            DB::table('postAmount')->insert(['email'=>Auth::user()->email]);
+            DB::table('postAmount')->where('email', Auth::user()->email)->increment('postAmount', 1);
+            DB::table('posts')->insert(['email'=> Auth::user()->email, 'postValue'=> $request->input('inputForm'), 'postAmount'=>$lastAmountValue, 'created_at'=>DB::raw('NOW()'), 'postAmount'=>1]);
+    }
+//$posts = new posts();
+return redirect()->to('profile');
+
+}
+}
---
+++ project/resources/js/Pages/Profile/profile.vue
@@ -0,0 +1,82 @@
+
+<!----><template>
+    <AuthenticatedLayout  >
+      <template #header>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Profile</h2>
+      </template>
+      <div class="flex h-screen flex-col items-center">
+        <button id="buttonPostCreate" class="dark:text-gray-200 font-semibold text-[30px]">CREATE POST</button>
+<!--    <textarea class="a   break-word whitespace-pre-wrap  " ></textarea>-->
+     <!--<div class="post flex  mt-20 bg-white h-[100px] w-[300px]" v-for="post in posts">{{ post.postValue }}</div>-->
+      <textarea class="a   break-word whitespace-pre-wrap m-2 h-[100px]] w-[550px] " v-for="post in posts">{{ post.postValue }}</textarea>
+    </div>
+
+
+
+
+
+
+
+    </AuthenticatedLayout>
+  </template>
+
+
+
+
+
+
+
+
+<!----><script >
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        this.fetchPosts()
+    },
+    methods: {
+        fetchPosts() {
+    axios.get('api/profile')
+        .then(response => {
+            this.posts = response.data;
+        })
+        .catch(error => console.log(error))
+}
+    }
+}
+</script>
+
+
+<script setup>
+import { onMounted } from 'vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import axios from 'axios';
+
+
+onMounted(() => {
+  const postCreate = document.getElementById('buttonPostCreate');
+
+
+
+
+
+
+
+postCreate.addEventListener('click', function (e) {
+e.preventDefault();
+    postCreate.insertAdjacentHTML('afterend', '        <form class="form flex flex-row" id="formSubmit" action="postSubmision"   >     <textarea name="inputForm" class="input  h-[150px] w-[550px]" type="text"></textarea> <input class="bg-white h-[150px]" type="submit"></form>')
+   //postCreate.insertAdjacentHTML('afterend','<div class="post bg-white h-[100px] w-[300px]"></div>')
+    postCreate.disabled = true;
+const formSubmit = document.getElementById('formSubmit');
+formSubmit.addEventListener('click', function(e){
+}
+)
+  });
+});
+</script>
+-->
